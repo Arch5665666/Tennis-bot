16de0be893ce02d6fe8a9f5ab58efa8019e6a31b
Arch5665666 <pertinirt123@mail.ru>
2026-06-05 14:26:25 +0300

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,22 @@
+import requests
+from datetime import datetime
+
+BOT_TOKEN = "8711575445:AAFA2iJ9ZAUR0Mz5hd2XAGxPrJ02QMgszKc"
+CHAT_ID = "343523199"
+
+def send_telegram(text):
+    print("📤 Отправка...")
+    try:
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        r = requests.post(url, json={"chat_id": CHAT_ID, "text": text}, timeout=10)
+        if r.status_code == 200:
+            print("✅ Telegram отправлено")
+        else:
+            print(f"❌ Ошибка: {r.status_code}")
+    except Exception as e:
+        print(f"❌ Ошибка: {e}")
+
+if __name__ == "__main__":
+    print("🚀 ТЕСТОВЫЙ БОТ ЗАПУЩЕН!")
+    send_telegram("🔴 ТЕСТ: бот работает на GitHub Actions!")
+    print("✅ Завершено")
